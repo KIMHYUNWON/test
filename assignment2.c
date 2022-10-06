f94ea45d8f61ef0b1e3a265faca3d9dd9d16d580
dexter.won <khw73850@gmail.com>
2022-10-06 21:53:30 +0900

    add function exit

--- assignment2.c
+++ assignment2.c
@@ -1,10 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void exit(char* str) {
+
+	if (strcmp(str, "exit") == 0) {
+		exit(0);
+	}
+}
 
 int main(void) {
 	char str[10];
 
 	scanf("%s", str);
+	exit(str);
 
 	printf("%s\n", str);
 
